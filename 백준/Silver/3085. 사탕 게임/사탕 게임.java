0fd99734bc66bcf0228844f4a5ce9345a3162247
yookyeong <syg9929@naver.com>
2022-04-24 00:35:26 +0900

    [Silver III] Title: 사탕 게임, Time: 180 ms, Memory: 13356 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.java
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.java
@@ -1,102 +1,105 @@
 import java.util.Scanner;
 
 public class Main {
-	static int[][] map;
+	static int[][] candy;
 	static int N;
 	static int max = Integer.MIN_VALUE;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+
 		N = sc.nextInt();
 
-		map = new int[N][N];
+		candy = new int[N][N];
 
 		for (int i = 0; i < N; i++) {
 			String s = sc.next();
 			for (int j = 0; j < N; j++) {
-				switch (s.substring(j, j + 1)) {
-				case "C": // 빨
-					map[i][j] = 1;
+				switch (s.substring(j, j+1)) {
+				case "C": //빨강
+					candy[i][j] = 1;
 					break;
-				case "P": // 파
-					map[i][j] = 2;
+				case "P": //파랑
+					candy[i][j] = 2;
 					break;
-				case "Z": // 초
-					map[i][j] = 3;
+				case "Z": //초록
+					candy[i][j] = 3;
 					break;
-				case "Y": // 노
-					map[i][j] = 4;
+				case "Y":
+					candy[i][j] = 4;
+					break;
+				default:
 					break;
 				}
 			}
-		}
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (i != N - 1 & j == N - 1) {
-					// 아래와 스왑
-					int tmp = map[i][j];
-					map[i][j] = map[i + 1][j];
-					map[i + 1][j] = tmp;
-
-					// 맥스값찾기
+		}//입력완료
+		
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				//i가 마지막 줄이 아니고 j가 제일 오른쪽일때 아래만 검사
+				if(i != N-1 && j == N-1) {
+					int tmp = candy[i][j];
+					candy[i][j] = candy[i+1][j];
+					candy[i+1][j] = tmp;
+					
 					max_search();
-
-					// 원래대로 되돌리기
-					tmp = map[i][j];
-					map[i][j] = map[i + 1][j];
-					map[i + 1][j] = tmp;
-					continue;
+					
+					tmp = candy[i][j];
+					candy[i][j] = candy[i+1][j];
+					candy[i+1][j] = tmp;
 				}
-
-				else if (i == N - 1 && j != N - 1) {
-					int tmp = map[i][j];
-					map[i][j] = map[i][j + 1];
-					map[i][j + 1] = tmp;
-
+				//i가 마지막줄이고 j가 제일 오른쪽줄이 아닐때 오른쪽으로만 검사
+				else if(i == N-1 && j != N-1) {
+					int tmp = candy[i][j];
+					candy[i][j] = candy[i][j+1];
+					candy[i][j+1] = tmp;
+					
 					max_search();
-
-					tmp = map[i][j];
-					map[i][j] = map[i][j + 1];
-					map[i][j + 1] = tmp;
-					continue;
+					
+					tmp = candy[i][j];
+					candy[i][j] = candy[i][j+1];
+					candy[i][j+1] = tmp;
 				}
-
+				
+				//마지막칸 도착시 break
 				else if (i == N - 1 && j == N - 1)
 					break;
-
-				int tmp = map[i][j];
-				map[i][j] = map[i + 1][j];
-				map[i + 1][j] = tmp;
-
-				max_search();
-
-				tmp = map[i][j];
-				map[i][j] = map[i + 1][j];
-				map[i + 1][j] = tmp;
-
-				tmp = map[i][j];
-				map[i][j] = map[i][j + 1];
-				map[i][j + 1] = tmp;
-
-				max_search();
-
-				tmp = map[i][j];
-				map[i][j] = map[i][j + 1];
-				map[i][j + 1] = tmp;
-
+				
+				//그 외의 경우엔 오른쪽, 아래 다 검사
+				else {
+					
+					int tmp = candy[i][j];
+					candy[i][j] = candy[i+1][j];
+					candy[i+1][j] = tmp;
+					
+					max_search();
+					
+					tmp = candy[i][j];
+					candy[i][j] = candy[i+1][j];
+					candy[i+1][j] = tmp;
+					
+					tmp = candy[i][j];
+					candy[i][j] = candy[i][j+1];
+					candy[i][j+1] = tmp;
+					
+					max_search();
+					
+					tmp = candy[i][j];
+					candy[i][j] = candy[i][j+1];
+					candy[i][j+1] = tmp;
+				}
 			}
-
 		}
 		System.out.println(max);
-
 	}
 
 	private static void max_search() {
 		int cnt = 1;
+		//가로검사
 		out: for (int i = 0; i < N; i++) {
 			cnt = 1;
 			for (int j = 0; j < N - 1; j++) {
-				if (map[i][j] == map[i][j + 1]) {
+				if (candy[i][j] == candy[i][j + 1]) {
 					cnt++;
 					max = Math.max(max, cnt);
 					if (max == N)
@@ -107,11 +110,11 @@ public class Main {
 		}
 
 		cnt = 1;
-
+		//세로검사
 		out: for (int i = 0; i < N; i++) {
 			cnt = 1;
 			for (int j = 0; j < N - 1; j++) {
-				if (map[j][i] == map[j + 1][i]) {
+				if (candy[j][i] == candy[j + 1][i]) {
 					cnt++;
 					max = Math.max(max, cnt);
 					if (max == N)
@@ -121,6 +124,6 @@ public class Main {
 
 			}
 		}
-
+		
 	}
 }
